79e3378b6615efc399086d0f1d3d8d97a4ca4731
Brena Freitas <brena@alunos.utfpr.edu.br>
2024-06-17 23:50:58 -0300

    Add function: Search with name.

--- js/contatar.js
+++ js/contatar.js
@@ -82,6 +82,7 @@ function mostrarMensagens() {
         li.appendChild(mensagemSpan);
         li.appendChild(dataSpan);
 
+        //criar botão para excluir 
         var botaoExcluir = document.createElement("button");
         botaoExcluir.textContent = "X";
         botaoExcluir.classList.add("botao-excluir");
@@ -112,3 +113,45 @@ function removerContato(contato) {
 }
 
 mostrarMensagens();
+
+// função para pesquisar por nome 
+function pesquisarPorNome() {
+    var nomePesquisado = document.getElementById("pesquisa").value;
+
+    var itemLista = document.querySelector(".mostrar-mensagem ul");
+    itemLista.innerHTML = '';
+
+    contatos.forEach(function(contato) {
+        var contatoNome = contato.nome.toLowerCase();
+
+        if (contato.nome === nomePesquisado || contatoNome.startsWith(nomePesquisado.toLowerCase())) {
+
+            var itemLista = document.querySelector(".mostrar-mensagem ul");
+
+            var li = document.createElement("li");
+            var idSpan = document.createElement("span");
+            idSpan.textContent = `ID: ${contato.id}`;
+            var nomeSpan = document.createElement("span");
+            nomeSpan.textContent = `Nome: ${contato.nome}`;
+            nomeSpan.style.marginLeft = "30px";
+            var emailSpan = document.createElement("span");
+            emailSpan.textContent = `Email: ${contato.email}`;
+            emailSpan.style.marginLeft = "30px";
+            var mensagemSpan = document.createElement("span");
+            mensagemSpan.textContent = `Mensagem: ${contato.mensagem}`;
+            mensagemSpan.style.marginLeft = "30px";
+            li.appendChild(idSpan);
+            li.appendChild(nomeSpan);
+            li.appendChild(emailSpan);
+            li.appendChild(mensagemSpan);
+            itemLista.appendChild(li);
+        }
+    });
+}
+
+function limparContatos(){
+
+    contatos = []; 
+    localStorage.removeItem("contatos");
+}
+
